e68dae3bb2cd8c2a6b8d42b1ced10a7931222cb9
oliver mcrobbie <omcrobbie@gmail.com>
2016-02-13 16:24:22 +0000

    More sorting

--- sorting.js
+++ sorting.js
@@ -2,7 +2,6 @@ function bubbleSort(arr, count){
 count = count || 0;
   var swapped = false;
   for(var i=0;i<arr.length - count;i++){
-    bubbleSort.comparisons++;
     if (arr[i] > arr[i+1]){
       swap.call(arr,i, i+1);
       swapped = true;
@@ -18,12 +17,9 @@ function swap(n1,n2){
     var oldN1 = this[n1];
     this[n1] = this[n2];
     this[n2] = oldN1;
-    swap.timesRun++;
 
 }
 
-swap.timesRun = 0;
-bubbleSort.comparisons = 0;
 
 function merge(left,right){
   var arr = [];
@@ -38,7 +34,56 @@ function merge(left,right){
 
 }
 
-function splitArr(arr){
+function mergeSort(arr){
+  if (arr.length <= 1)
+    return arr;
   var length = Math.floor(arr.length /2);
-  return [arr.splice(0, length), arr];
+  var left = mergeSort(arr.splice(0, length));
+  var right = mergeSort(arr);
+  var rtn = merge(left,right);
+  return rtn;
+}
+//Not in-place
+// function quickSort(arr){
+//   debugger;
+//   if (!arr.length)
+//     return [];
+//   var left = [], right = [], pivot = arr[0];
+//   for (var i =1;i<arr.length;i++){
+//     if (arr[i]< pivot)
+//       left.push(arr[i]);
+//     else
+//       right.push(arr[i]);
+//   }
+//   return quickSort(left).concat(pivot,quickSort(right));
+// }
+
+//Sort of in place
+function quickSort(arr){
+  if (!arr.length)
+    return arr;
+  var pivot = 0;
+  for (var i =1;i<arr.length;i++){
+    if (arr[i]< arr[pivot])
+      swap.call(arr,i,pivot++);
+  }
+  return quickSort(arr.slice(0, pivot)).concat(arr[pivot],quickSort(arr.slice(pivot+1)));
+}
+
+function insertSort(arr){
+  for (var i in arr){
+    var temp = arr[i];
+    for (var j=i-1;j>=0 && (arr[j]>temp); j--)
+      arr[j+1] = arr[j];
+    arr[j+1] = temp;
+  }
+  return arr;
+}
+
+function heapSort(arr){
+  var last = arr.length -1;
+  var first = 0;
+  while (last-- > first++){
+    console.log(last,first);
+  }
 }
--- sortingSpec.js
+++ sortingSpec.js
@@ -2,6 +2,7 @@ var testArr = [5,2,1,3];
 var timesRun = 0;
 var comparisons = 0;
 
+
 describe('Bubble Sort', function(){
     it('handles an empty array', function(){
         expect( bubbleSort([]) ).toEqual( [] );
@@ -9,20 +10,36 @@ describe('Bubble Sort', function(){
     it('handles array with multiple elements', function(){
       expect(bubbleSort(testArr)).toEqual([1,2,3,5]);
     });
-    it ('correctly reports number of comparisons', function(){
-      bubbleSort(testArr);
-      expect(swap.timesRun).toEqual(4);
-      console.log(bubbleSort.comparisons);
-    });
+  
 });
 
 describe('Merge Sort', function(){
     it('is able to merge two sorted arrays', function(){
         expect(merge([1,5,10], [2,3,4,6])).toEqual([1,2,3,4,5,6,10]);
     });
-    it('is able to subdivide properly', function(){
-      var rtn = splitArr([1,2,3,4,5]);
-      expect(rtn[0].length).toEqual(2);
-      expect(rtn[1].length).toEqual(3);
+    
+    it('is able to merge sort', function() {
+        //spyOn(window, 'merge');
+        expect(mergeSort([2,1,5,10,3])).toEqual([1,2,3,5,10]);
+        //console.log(merge.callCount);
+        
     });
+});
+
+describe('Quick sort', function(){
+    it('is able to quicksort', function() {
+        expect(quickSort([2,1,10,5,3,8,7])).toEqual([1,2,3,5,7,8,10]);
+    });
+});
+
+describe("Insert sort", function(){
+   it('is able to insert sort properly', function(){
+      expect(insertSort([2,1,5,10,3])).toEqual([1,2,3,5,10]); 
+   });
+});
+
+describe("Heap sort", function(){
+   it("is able to heap sort", function(){
+      expect(heapSort([2,1,5,10,3])).toEqual([1,2,3,5,10]); 
+   }); 
 });
